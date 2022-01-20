2dbfae7d4f7e9d5c40658e3b4948fec68e822f6f
Yosa Rama Dinata <yosamelody07@gmail.com>
2022-01-20 14:51:11 +0800

    ADD: Component Testing

---
+++ tests/components/button.test.js
@@ -0,0 +1,9 @@
+import CustomButton from "components/button";
+import { render, screen } from "@testing-library/react";
+
+describe("Button Component", () => {
+  test("should get children", () => {
+    render(<CustomButton type="red">This Button</CustomButton>);
+    expect();
+  });
+});
